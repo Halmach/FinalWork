b66e259c11b6136b24566cea77385d77aa860ca1
Khalmatov <large000@yandex.ru>
2021-11-19 22:38:21 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слагаемые:");
+            try
+            {
+                ICalculator calc = new Calculator();
+                int result = calc.Addition(Convert.ToInt32(Console.ReadLine()), 
+                              Convert.ToInt32(Console.ReadLine()));
+                Console.WriteLine($"Результат: {result}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+
+    interface ICalculator
+    {
+        int Addition(int a, int b);
+    }
+
+    class Calculator:ICalculator
+    {
+        public int Addition(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
